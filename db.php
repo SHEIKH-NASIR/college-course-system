b37161b3e47998fc19d7a0676adbe0e757b5b2b2
SHEIKH-NASIR <sheikhmohdnasir2802@gmail.com>
2026-03-03 21:12:58 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,15 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "college_course_system");
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$host = "dpg-d6jes18gjchc739jgn70-a";
+$port = "5432";
+$dbname = "college_course_system";
+$user = "college_course_system_user";
+$password = "lHgOV1OaCxdMVJQvy9EhjIeEdPPxXOp9";
+
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if(!$conn){
+    die("Database connection failed.");
 }
+
 ?>
